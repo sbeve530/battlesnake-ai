b63dff9df0de58b13e055c06fcbda7ea44ee68f5
yt7409 <jannesp03@gmail.com>
2024-06-28 23:48:36 +0200

    refactoring

--- simple_game_state.py
+++ simple_game_state.py
@@ -6,10 +6,12 @@ class SimpleGameState:
     def __init__(self, game_state = None):
         if game_state is None:
             return
+
         self.x_size = game_state["board"]["width"]
         self.y_size = game_state["board"]["height"]
-        self.snakes = [snake["body"] for snake in game_state["board"]["snakes"]] # your body is snake with index 0, head is dict in snake body with index 0
-        self.foods = [game_state["board"]["food"]]
+        self.snakes = [[snake["id"], snake["body"]] for snake in game_state["board"]["snakes"]] # head is body with index 0
+        self.foods = game_state["board"]["food"]
+        self.player = game_state["you"]["id"]
         self.is_temp = False
 
     def copy(self):
@@ -31,13 +33,13 @@ class SimpleGameState:
             "right"
         ]
         snake_head = self.snakes[snake_index][0]
-        all_snake_bodies = [snake_cell for snake_cell in self.snakes]
+        all_snake_bodies = [snake_cell for snake_cells in self.snakes for snake_cell in snake_cells[1]]
 
         if snake_head["x"] == 0 or {"x": snake_head["x"] - 1, "y": snake_head["y"]} in all_snake_bodies:
             possible_moves.pop(2)
         elif snake_head["x"] == self.x_size - 1 or {"x": snake_head["x"] + 1, "y": snake_head["y"]} in all_snake_bodies:
             possible_moves.pop(3)
-        if snake_head["y"] == 0 or {"x": snake_head["x"], "y": snake_head["y"] - 1} in all_snake_bodies:
+        elif snake_head["y"] == 0 or {"x": snake_head["x"], "y": snake_head["y"] - 1} in all_snake_bodies:
             possible_moves.pop(1)
         elif snake_head["y"] == self.y_size - 1 or {"x": snake_head["x"], "y": snake_head["y"] + 1} in all_snake_bodies:
             possible_moves.pop(0)
@@ -48,23 +50,101 @@ class SimpleGameState:
     def next_move(self, moves:list[str]):
         """Calculates next state given moves for all snakes.
         :param moves: A list of moves for all snakes
-        :return: new updated state and eliminated snakes
+        :return: new updated state, eliminated snakes
         """
-        tmp_state = self.copy()
-        eliminated_snakes = [] # indices of all eliminated snakes
+        new_state = self.copy()
+
+        all_safe_moves = [] # safe moves for snakes in initial state
         for i, snake in enumerate(self.snakes):
-            if len(tmp_state.get_safe_moves(i)) == 0:
-                pass
+            all_safe_moves.append(self.get_safe_moves(i))
 
-
+        # add new heads
+        heads = []
         for i, move in enumerate(moves):
             if move == "up":
-                tmp_state.snakes[i].insert(0, {"x": self.snakes[i][0]["x"], "y": self.snakes[i][0]["y"] + 1})
+                new_state.snakes[i].insert(0, {"x": self.snakes[i][0]["x"], "y": self.snakes[i][0]["y"] + 1})
+                heads.append({"x": self.snakes[i][0]["x"], "y": self.snakes[i][0]["y"] + 1})
             elif move == "down":
-                tmp_state.snakes[i].insert(0, {"x": self.snakes[i][0]["x"], "y": self.snakes[i][0]["y"] - 1})
+                new_state.snakes[i].insert(0, {"x": self.snakes[i][0]["x"], "y": self.snakes[i][0]["y"] - 1})
+                heads.append({"x": self.snakes[i][0]["x"], "y": self.snakes[i][0]["y"] - 1})
             elif move == "left":
-                tmp_state.snakes[i].insert(0, {"x": self.snakes[i][0]["x"] - 1, "y": self.snakes[i][0]["y"]})
+                new_state.snakes[i].insert(0, {"x": self.snakes[i][0]["x"] - 1, "y": self.snakes[i][0]["y"]})
+                heads.append({"x": self.snakes[i][0]["x"] - 1, "y": self.snakes[i][0]["y"]})
             elif move == "right":
-                tmp_state.snakes[i].insert(0, {"x": self.snakes[i][0]["x"] + 1, "y": self.snakes[i][0]["y"]})
+                new_state.snakes[i].insert(0, {"x": self.snakes[i][0]["x"] + 1, "y": self.snakes[i][0]["y"]})
+                heads.append({"x": self.snakes[i][0]["x"] + 1, "y": self.snakes[i][0]["y"]})
 
-        return tmp_state, eliminated_snakes
+        # check eliminations
+        eliminated_snakes = [] # indices of all eliminated snakes
+
+        for i, snake in enumerate(new_state.snakes):
+            if len(all_safe_moves[i]): # check border and body collisions
+                eliminated_snakes.append(i)
+                break
+            #if snake["body"][0] in
+
+
+        # check food consumptions
+
+
+
+        return new_state, eliminated_snakes
+
+    def next_state_2(self, moves: [[str, str]]):
+        """Calculates next state given moves for all snakes.
+        :param moves: A list of moves for all snakes, e.g. [["id-1", "up"], ["id-2", "down"]]
+        :return: if self dies, then empty state, else new state"""
+        new_state = self.copy()
+
+        # order snakes by length
+        new_state.snakes.sort(key = lambda tup: len(tup[1]), reverse = True)
+
+        # 1. apply moves (add new heads and pop tails)
+        for snake in new_state.snakes:
+            move = next((move[1] for move in moves if move[0] == snake[0]), "down")
+            if move == "up":
+                snake[1].insert(0, {"x": snake[1][0]["x"], "y": snake[1][0]["y"] + 1})
+                snake[1].pop()
+            elif move == "down":
+                snake[1].insert(0, {"x": snake[1][0]["x"], "y": snake[1][0]["y"] - 1})
+                snake[1].pop()
+            elif move == "left":
+                snake[1].insert(0, {"x": snake[1][0]["x"] - 1, "y": snake[1][0]["y"]})
+                snake[1].pop()
+            elif move == "right":
+                snake[1].insert(0, {"x": snake[1][0]["x"] + 1, "y": snake[1][0]["y"]})
+                snake[1].pop()
+
+        # 2. reduce snake health
+
+        # 3. feed snakes (go through ordered snake list and feed the longest snake, reset health)
+        for food in new_state.foods:
+            fed_snake = next((snake for snake in new_state.snakes if snake[1][0] == food), None)
+            if fed_snake:
+                new_state.foods.remove(food)
+                # TODO: append tail
+
+        # 4. TODO: elimination (eliminate starved or deserted snakes, check collisions, apply eliminations)
+
+    #def order_snakes(self):
+    #    """Sorts snakes by length (descending) to allow for easy feeding later.
+    #    """
+    #    self.snakes.sort(key = lambda tup: len(tup[1]), reverse = True)
+
+    #def apply_moves(self, moves: [[str, str]]):
+    #    """Applies moves to all snakes by creating new head and popping the tail for each snake.
+    #    :param moves: A list of moves for all snakes, e.g. [["id-1", "up"], ["id-2", "down"]]"""
+    #    for snake in self.snakes:
+    #        move = next((move[1] for move in moves if move[0] == snake[0]), "down")
+    #        if move == "up":
+    #            snake[1].insert(0, {"x": snake[1][0]["x"], "y": snake[1][0]["y"] + 1})
+    #            snake[1].pop()
+    #        elif move == "down":
+    #            snake[1].insert(0, {"x": snake[1][0]["x"], "y": snake[1][0]["y"] - 1})
+    #            snake[1].pop()
+    #        elif move == "left":
+    #            snake[1].insert(0, {"x": snake[1][0]["x"] - 1, "y": snake[1][0]["y"]})
+    #            snake[1].pop()
+    #        elif move == "right":
+    #            snake[1].insert(0, {"x": snake[1][0]["x"] + 1, "y": snake[1][0]["y"]})
+    #            snake[1].pop()
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-
+from simple_game_state import *
 
 test_game_state = {
     "game": {
@@ -21,24 +21,23 @@ test_game_state = {
         "height": 11,
         "width": 11,
         "food": [
-            {"x": 5, "y": 5},
-            {"x": 9, "y": 0},
-            {"x": 2, "y": 6}
+            {"x": 2, "y": 6},
+            {"x": 0, "y": 0},
         ],
         "hazards": [
         ],
         "snakes": [
             {
-                "id": "snake-508e96ac-94ad-11ea-bb37",
+                "id": "snake-0",
                 "name": "My Snake",
                 "health": 100,
                 "body": [
-                    {"x": 0, "y": 0},
-                    {"x": 1, "y": 0},
-                    {"x": 2, "y": 0}
+                    {"x": 2, "y": 5},
+                    {"x": 2, "y": 4},
+                    {"x": 2, "y": 3}
                 ],
                 "latency": "111",
-                "head": {"x": 0, "y": 0},
+                "head": {"x": 2, "y": 5},
                 "length": 3,
                 "shout": "why are we shouting??",
                 "customizations":{
@@ -48,17 +47,36 @@ test_game_state = {
                 }
             },
             {
-                "id": "snake-b67f4906-94ae-11ea-bb37",
+                "id": "snake-1",
                 "name": "Another Snake",
-                "health": 16,
+                "health": 100,
                 "body": [
-                    {"x": 5, "y": 4},
-                    {"x": 5, "y": 3},
-                    {"x": 6, "y": 3},
-                    {"x": 6, "y": 2}
+                    {"x": 2, "y": 2},
+                    {"x": 3, "y": 2},
+                    {"x": 4, "y": 2}
                 ],
                 "latency": "222",
-                "head": {"x": 5, "y": 4},
+                "head": {"x": 2, "y": 2},
+                "length": 3,
+                "shout": "I'm not really sure...",
+                "customizations":{
+                    "color":"#26CF04",
+                    "head":"silly",
+                    "tail":"curled"
+                }
+            },
+            {
+                "id": "snake-2",
+                "name": "Another Snake",
+                "health": 100,
+                "body": [
+                    {"x": 6, "y": 5},
+                    {"x": 7, "y": 5},
+                    {"x": 8, "y": 5},
+                    {"x": 9, "y": 5}
+                ],
+                "latency": "222",
+                "head": {"x": 5, "y": 5},
                 "length": 4,
                 "shout": "I'm not really sure...",
                 "customizations":{
@@ -66,20 +84,39 @@ test_game_state = {
                     "head":"silly",
                     "tail":"curled"
                 }
-            }
+            },
+            {
+                "id": "snake-3",
+                "name": "Another Snake",
+                "health": 100,
+                "body": [
+                    {"x": 5, "y": 6},
+                    {"x": 5, "y": 7},
+                    {"x": 5, "y": 8}
+                ],
+                "latency": "222",
+                "head": {"x": 5, "y": 6},
+                "length": 3,
+                "shout": "I'm not really sure...",
+                "customizations":{
+                    "color":"#26CF04",
+                    "head":"silly",
+                    "tail":"curled"
+                }
+            },
         ]
     },
     "you": {
         "id": "snake-508e96ac-94ad-11ea-bb37",
         "name": "My Snake",
-        "health": 54,
+        "health": 100,
         "body": [
-            {"x": 0, "y": 0},
-            {"x": 1, "y": 0},
-            {"x": 2, "y": 0}
+            {"x": 2, "y": 5},
+            {"x": 2, "y": 4},
+            {"x": 2, "y": 3}
         ],
         "latency": "111",
-        "head": {"x": 0, "y": 0},
+        "head": {"x": 2, "y": 5},
         "length": 3,
         "shout": "why are we shouting??",
         "customizations": {
@@ -90,6 +127,10 @@ test_game_state = {
     }
 }
 
-test_moves = [
-    "up", "up"
-]
+test_moves = [["snake-0", "up"], ["snake-1", "up"], ["snake-2", "left"], ["snake-3", "down"]]
+
+test_simple_game_state = SimpleGameState(test_game_state)
+
+test_simple_game_state.next_state_2(test_moves)
+
+print(test_simple_game_state.snakes)
