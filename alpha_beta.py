4e5971dc3aad64122e99b5f58567bb40a4d02c4b
yt7409 <jannesp03@gmail.com>
2024-06-30 18:13:56 +0200

    adjust for testing in replit

--- alpha_beta.py
+++ alpha_beta.py
@@ -8,6 +8,14 @@ import time
 
 
 def alpha_beta_decision(game_state: Dict, depth: int) -> Dict:
+    """Calculates the best move by exploring possible paths up to a specified depth.
+    Rates the final states using a utility function and then backtracks, storing the worst outcome for each path.
+    At depth=1, it selects the move that provides the best of these worst outcomes.
+    It is not assumed, that opponents play optimal, but rather that they play safe.
+    :param game_state: The game state to calculate the best move for.
+    :param depth: The depth of the game state.
+    :return: The best move based on the given game state and used utility function.
+    """
     if depth < 1:
         return {"move": "down"}
     initial_state = SimpleGameState(game_state)
@@ -16,7 +24,7 @@ def alpha_beta_decision(game_state: Dict, depth: int) -> Dict:
     best_move = None
     best_value = -math.inf
 
-    #all_moves_each_snake = [[[snake[0], move] for move in initial_state.get_safe_moves(snake_index)] for
+    # all_moves_each_snake = [[[snake[0], move] for move in initial_state.get_safe_moves(snake_index)] for
     #                        snake_index, snake in enumerate(initial_state.snakes)]
 
     # all possible moves for every snake that isn't the player
@@ -26,17 +34,17 @@ def alpha_beta_decision(game_state: Dict, depth: int) -> Dict:
 
     # for each possible move of player, get min of all following paths
     for safe_move in initial_state.get_safe_moves(player_index):
-        print(safe_move)
+        #print(safe_move)
         cartesian = snakes_moves_cartesian(all_moves_each_np_snake)
         for move_list in cartesian:
             foobar = math.inf
-            move_list.insert(0, [initial_state.player_id, safe_move]) # insert player move to cartesian
-            print("\t" + str(move_list))
+            move_list.insert(0, [initial_state.player_id, safe_move])  # insert player move to cartesian
+            #print("\t" + str(move_list))
             successor_state = initial_state.next_state(move_list)
             foo = utility(successor_state)
             if foo < foobar:
                 foobar = foo
-            print("\t" + str(foo))
+            #print("\t" + str(foo))
         if foobar > best_value:
             best_value = foobar
             best_move = safe_move
@@ -55,24 +63,7 @@ def alpha_beta_decision(game_state: Dict, depth: int) -> Dict:
     #        best_value = value
     #        best_move = moves[0]
 
-    ###
-
-    # print(all_moves_each_snake)
-    # print("kart. product test:")
-    # print(snakes_moves_cartesian(all_moves_each_snake))
-    # print("length: " + str(len(snakes_moves_cartesian(all_moves_each_snake))))
-
     return {"move": best_move} if best_move else {"move": "down"}
-
-
-def snakes_moves_cartesian(moves_per_snake: list):
-    """
-    :param moves_per_snake: list of lists of tuples of snake_id and move
-    :return: cartesian product of all moves per snake
-    """
-    combinations = list(product(*moves_per_snake))
-    combinations = [list(combo) for combo in combinations]
-    return combinations
 
 
 def max_value(simplified_game_state, alpha, beta, depth) -> float:  # TODO: adapt to simple_game_state.py
@@ -88,18 +79,15 @@ def max_value(simplified_game_state, alpha, beta, depth) -> float:  # TODO: adap
     return value
 
 
-def min_value(game_state, alpha, beta, depth) -> float:  # TODO: adapt to simple_game_state.py
-    if depth < 1 or is_terminal(game_state):
-        foo = utility(game_state)
-        print(foo)
-        return foo
-    value = math.inf
-    for moves, new_state in successors(game_state):
-        value = min(value, max_value(new_state, alpha, beta, depth - 1))
-        if value <= alpha:
-            return value
+def min_value(current_game_state: SimpleGameState, alpha, beta, depth) -> float:  # TODO: adapt to simple_game_state.py
+    if depth < 1 or is_terminal(current_game_state):
+        return utility(current_game_state)
 
-        beta = min(beta, value)
+    value = math.inf
+    # for moves, new_state in successors(simplified_game_state):
+    #    value = min(value, max_value(new_state, alpha, beta, depth - 1))
+    #    if value <= alpha:
+    #        return value
 
     return value
 
@@ -118,6 +106,17 @@ def min_value(game_state, alpha, beta, depth) -> float:  # TODO: adapt to simple
 #            utility_value += 3
 #    utility_value += total_inverse_distance / len(food_sources)
 #    return utility_value
+
+
+def snakes_moves_cartesian(moves_per_snake: list):
+    """
+    :param moves_per_snake: list of lists of tuples of snake_id and move
+    :return: cartesian product of all moves per snake
+    """
+    combinations = list(product(*moves_per_snake))
+    combinations = [list(combo) for combo in combinations]
+    return combinations
+
 
 def utility(simplified_game_state: SimpleGameState) -> float:
     """Calculates the utility value of a given game state.
@@ -141,9 +140,13 @@ def utility(simplified_game_state: SimpleGameState) -> float:
     utility_value += 1 / min_distance
     return utility_value
 
+
 def food_distance(head_pos, food_pos) -> float:
     return abs(head_pos["x"] - food_pos["x"]) + abs(head_pos["y"] - food_pos["y"])
 
+
 # TODO: check if next_state returns []
-def is_terminal(simplified_game_state: Dict) -> bool:
+def is_terminal(simplified_game_state: SimpleGameState) -> bool:
+    if simplified_game_state == []:
+        return True
     return False
